c39b11030c21c94bef0c786274591c7f1ba4b19d
dscx <dnchmbrln@gmail.com>
2014-08-18 20:21:26 -0700

    added basic schema

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -4,10 +4,23 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  /* objectId */
+  `objectId` INT NOT NULL AUTO_INCREMENT,
+  `text` varchar,
+  `message` varchar,
+  `userId` int,
+  `roomname` varchar,
+  `createdAt` datetime,
+  PRIMARY KEY  (`objectId`)
+  FOREIGN KEY ('userId') REFERENCES users('userId')
 );
 
 /* Create other tables and define schemas for them here! */
-
+CREATE TABLE users (
+  'userId' INT NOT NULL AUTO_INCREMENT,
+  'username' varchar,
+  PRIMARY KEY ('userId')
+);
 
 
 
